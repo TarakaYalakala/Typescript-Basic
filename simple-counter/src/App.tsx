6416f3370a12ef3f1ea5bff70a44f28dc92d429f
unknown <tarakayalakala@gmail.com>
2024-10-08 15:19:09 +0530

    second  commit source

--- simple-counter/src/App.tsx
+++ simple-counter/src/App.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 function App() {
+
+
+const [data, setdata] = useState <number>(0);
+
+const handleInc = ():number | string => {
+  setdata(data + 1);
+  return data;
+}
+
+
+const handleDec = (): number => {
+   setdata(data - 1);
+   return data;
+
+}
+
   return (
-    <div>App</div>
+    <div>
+      <div className="container">
+        <button onClick={handleInc}>+</button>
+        <h2>{data}</h2>
+        <button onClick={handleDec}>-</button>
+
+      </div>
+    </div>
   )
 }
 
