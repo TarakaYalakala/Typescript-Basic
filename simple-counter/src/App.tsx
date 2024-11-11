2516bd749a09d60a67af2d7dd85deee874338ec7
unknown <tarakayalakala@gmail.com>
2024-11-11 15:49:18 +0530

    added class

--- simple-counter/src/App.tsx
+++ simple-counter/src/App.tsx
@@ -6,6 +6,23 @@ function App() {
 
 const [data, setdata] = useState <number>(0);
 
+const [obj, setobj] = useState<number>(0);
+
+class token {
+  constructor(public jwt: number) {
+
+  }
+
+  set tokens(val: number) {
+    this.jwt = val;
+  }
+}
+
+const tokenObj = new token(0);
+tokenObj.tokens = data;
+
+
+
 const handleInc = ():number | string => {
   setdata(data + 1);
   return data;
@@ -23,6 +40,7 @@ const handleDec = (): number => {
       <div className="container">
         <button onClick={handleInc}>+</button>
         <h2>{data}</h2>
+        <h3>DataSetter:{tokenObj.jwt}:</h3>
         <button onClick={handleDec}>-</button>
 
       </div>
